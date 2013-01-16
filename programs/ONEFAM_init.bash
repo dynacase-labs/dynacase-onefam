87dcb29fa43762fd9317331c53bec342122fbdce
Nicolas <nicolas.thing-leoh@anakeen.com>
2013-01-16 14:54:39 +0100

    Create dynacase-onefam module (closes #3485)

---
+++ programs/ONEFAM_init.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pgservice_core=`"$WIFF_ROOT"/wiff --getValue=core_db`
+
+if [ -z "$pgservice_core" ]; then
+    echo "Undefined or empty pgservice_core!"
+    exit 1
+fi
+
+PGSERVICE="$pgservice_core" psql --set ON_ERROR_STOP=on -f - <<EOF
+-- Activate the ONEFAM application
+UPDATE application SET available = 'Y' WHERE name = 'ONEFAM';
+EOF
+RET=$?
+if [ $RET -ne 0 ]; then
+    echo "Error hiding application 'ONEFAM'."
+    exit $RET
+fi
