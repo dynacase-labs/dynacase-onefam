43f55db62b078fb66b5eb852cd0f0ce8270bacb3
Jérôme Augé <jerome.auge@anakeen.com>
2016-04-08 11:51:56 +0200

    refactor(license): Changed license to GNU Lesser General Public License v3.0 (refs #6255)

--- Actions/onefam_root.php
+++ Actions/onefam_root.php
@@ -1,7 +1,6 @@
 <?php
 /*
  * @author Anakeen
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  * @package FDL
 */
 /**
@@ -9,7 +8,6 @@
  *
  * @author Anakeen
  * @version $Id: onefam_root.php,v 1.9 2008/04/18 09:47:38 eric Exp $
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  * @package FDL
  * @subpackage
  */
--- ONEFAM_EXT/Actions/onefam_ext_modpref.php
+++ ONEFAM_EXT/Actions/onefam_ext_modpref.php
@@ -1,7 +1,6 @@
 <?php
 /*
  * @author Anakeen
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  * @package FDL
 */
 /**
@@ -9,7 +8,6 @@
  *
  * @author Anakeen
  * @version $Id: onefam_modpref.php,v 1.8 2006/10/05 09:22:38 eric Exp $
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  * @package FDL
  * @subpackage
  */
